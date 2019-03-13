e5fef6f01d8e682e6528f0570f8676b361f9aba4
Nikita <nekitvand@gmail.com>
2019-03-13 19:35:32 +0300

    add chrome options

--- Application/WikiApp.py
+++ Application/WikiApp.py
@@ -5,8 +5,10 @@ from Pages.WikiMainPage import WikiMainHepler
 class App:
 
     def __init__(self):
-        self.driver = webdriver.Chrome()
+        browser_options = self.browser_options(options='start-maximized')
+        self.driver = webdriver.Chrome(chrome_options=browser_options)
         self.wiki = WikiMainHepler(self)
+        self.driver.implicitly_wait(10)
 
     def open_wiki_page(self):
         driver = self.driver
@@ -14,4 +16,15 @@ class App:
 
     def destroy(self):
         self.driver.quit()
+
+    def browser_options(self, options):
+        chrome_options = webdriver.ChromeOptions()
+        if chrome_options.add_argument(options) == 'headless':
+            return chrome_options
+        else:
+            chrome_options.add_argument("start-maximized")
+            return chrome_options
+
+
+
 
--- README.md
+++ README.md
@@ -5,3 +5,4 @@ Check this:
 * https://vk.com/@qa_ninjas-avtomatizaciya-testirovaniya-s-pomoschu-pythonpage-object-i -- Fixture + PageObject 
 * https://vk.com/@qa_ninjas-avtomatizaciya-testirovaniya-s-pomoschu-python-asserts-i-dru -- Asserts
 * https://vk.com/@qa_ninjas-avtomatizaciya-testirovaniya-s-pomoschu-pythonpytestmarks -- Pytest.Mark
+* https://vk.com/@qa_ninjas-avtomatizaciya-testirovaniya-s-pomoschu-pythonsommand-line-o -- Chrome Options
