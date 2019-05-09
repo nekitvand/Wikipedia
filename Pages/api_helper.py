a08cf54d146bf11cb207469012a88f807b2e61db
Nikita <nekitvand@gmail.com>
2019-05-10 01:28:01 +0300

    add api test
    Signed-off-by: Nikita <nekitvand@gmail.com>

---
+++ Pages/api_helper.py
@@ -0,0 +1,18 @@
+import requests
+import pytest
+import json
+
+class Facts:
+
+    def __init__(self,facts):
+        self.facts = facts
+
+    def cat(self):
+        query = {"animal_type":"cat","amount":"2"}
+        response = requests.get("https://cat-fact.herokuapp.com/facts/random", params=query)
+        response_response = response.json()
+        assert response.status_code == 200
+        assert response_response[0]["type"] == "cat"
+        return response.json()
+
+
