63a50a26d42043697c969c6d85770dba9a8ea90f
Nikita <nekitvand@gmail.com>
2019-05-10 21:31:15 +0300

    refactor api tests

--- Tests/test_api.py
+++ Tests/test_api.py
@@ -9,3 +9,9 @@ def test_dog_facts(api):
     dog_facts = api.facts.dog()
     assert dog_facts[0]["type"] == "dog" and dog_facts [1]["type"] == "dog"
     assert dog_facts[0]["text"] != '' or None and dog_facts [1]["text"] != '' or None
+
+
+def test_yandex_translate(api):
+    translate = api.facts.yandex_translate()
+    assert translate["text"] != '' or None
+
