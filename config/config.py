5da743dc0e92836255054e6f2f727ce5e9eb34f3
Nikita <nekitvand@gmail.com>
2019-06-01 19:49:17 +0300

    add new path for config from travis
    Signed-off-by: Nikita <nekitvand@gmail.com>

--- config/config.py
+++ config/config.py
@@ -3,12 +3,12 @@ import os
 
 
 def load_config():
-    with open(os.path.abspath(r'..\Wikipedia\qa_data.json'), encoding="UTF-8") as f:
+    with open(os.path.abspath('../Wikipedia/qa_data.json'), encoding="UTF-8") as f:
             json_data = json.load(f)
             qa_data_dict = json_data["test_data"]
     return qa_data_dict
 
 def load_animal_facts():
-    with open(os.path.abspath(r'..\Wikipedia\animal_facts.json'),encoding="UTF-8") as f:
+    with open(os.path.abspath('../Wikipedia/animal_facts.json'),encoding="UTF-8") as f:
         json_data = json.load(f)
     return json_data
